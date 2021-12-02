8d52750f5ac9c9a8b41e51500b54b657bcefd7fc
Ali Cigdemoglu <33742088+Alicigdemoglu@users.noreply.github.com>
2021-12-02 16:05:22 -0500

    Update the route for card to pdf

--- myapp/routes.py
+++ myapp/routes.py
@@ -12,12 +12,13 @@ from myapp.noteforms import NoteForm
 from myapp.models import User, Notes, Todo, Tracker, FlashCard
 from myapp.searchforms import SearchForm
 from myapp.registerforms import RegisterForm
-from flask import render_template, escape, flash, redirect,request
+from flask import render_template, escape, flash, redirect,request, send_file
 from markdown import markdown
 from flask_login import UserMixin,login_user,LoginManager,login_required,logout_user,current_user
 from myapp.todoforms import ToDo
 from myapp.workhrs import Track
 from datetime import datetime
+from io import BytesIO
 import pdfkit
 from werkzeug.utils import secure_filename
 from myapp.GetFile import GetFile
@@ -259,12 +260,7 @@ def card_to_pdf():
         Returns:
             return pdf file
     '''
-    form = GetFile()
-    if form.validate_on_submit():
-        name = secure_filename(form.file.data.name)
-        form.file.data.save('myapp/static/cards/' +name)
-        input_name= 'myapp/static/cards/' +name
-        output_name=input_name.split(".html")[0] + ".pdf"
-        pdfkit.from_file(input_name, output_name)
-        return render_template('cardtopdf.html', form=form, pdf=output_name)
-    return render_template('cardtopdf.html', form=form)
+
+    file_data = FlashCard.query.filter_by(user_id=current_user.id).first()
+    b = bytes(file_data.content, 'utf-8')
+    return send_file(file_data, attachment_filename = "cards.pdf", as_attachment=True)
